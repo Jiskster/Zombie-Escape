44634fe137e9180be407523817d5f7d6bb9c9695
Blur <danielthesmartypants2017@gmail.com>
2023-03-21 22:53:47 -0500

    more init changes

--- src/init.lua
+++ src/init.lua
@@ -2,6 +2,38 @@ for _, filename in ipairs{
 -- GunMod (RS NEO Setup)
 
 	
+	"2-ZECore/1-Init/int_freeslot.lua",
+	"2-ZECore/1-Init/int_global.lua",
+	"2-ZECore/1-Init/int_ZE_RSNEO.lua",
+
+	"2-ZECore/2-Console/con_timer.lua",
+	"2-ZECore/2-Console/con_rhealth.lua",
+	"2-ZECore/2-Console/con_cmds.lua",
+	
+		
+	"2-ZECore/3-Library/lib_ZE_main.lua",
+	"2-ZECore/3-Library/lib_ZE_rhealth.lua",
+	"2-ZECore/3-Library/lib_ZE_rhealth_dmg.lua",
+	"2-ZECore/3-Library/lib_ZE_player_cfg.lua",
+	"2-ZECore/3-Library/lib_ZE_player_rhealth.lua",
+	"2-ZECore/3-Library/lib_ZE_player_misc.lua",
+	"2-ZECore/3-Library/lib_ZE_mapload.lua",
+	"2-ZECore/3-Library/lib_ZE_netvars.lua",
+	"2-ZECore/3-Library/lib_ZE_MobjCollide.lua",
+	"2-ZECore/3-Library/lib_ZE_propspawn.lua",
+	"2-ZECore/3-Library/lib_ZE_radar.lua",
+	"2-ZECore/3-Library/lib_ZE_ExtraChars.lua",
+	"2-ZECore/3-Library/lib_ZE_Ability_Milne.lua",
+	"2-ZECore/3-Library/lib_ZE_ExtraChars_exec.lua",
+	"2-ZECore/3-Library/lib_ZE_rhealth_Extra.lua",
+	"2-ZECore/3-Library/lib_ZE_Revenger.lua",
+	"2-ZECore/3-Library/lib_ZE_swarmhud.lua",
+	"2-ZECore/3-Library/lib_ZE_antibounce.lua",
+
+	"2-ZECore/4-Execute/Exec_ZE_Player.lua",
+	"2-ZECore/4-Execute/Exec_ZE_Main.lua",
+	"2-ZECore/4-Execute/Exec_ZE_HUD.lua",
+
 	"4-ServerCore/1-MapVote/1_Rawset.lua",
 	"4-ServerCore/1-MapVote/2_Console.lua",
 	"4-ServerCore/1-MapVote/3_DebugFunctions.lua",
@@ -9,12 +41,6 @@ for _, filename in ipairs{
 	"4-ServerCore/1-MapVote/5_HUD.lua",
 	"4-ServerCore/1-MapVote/6_Main.lua",
 	"4-ServerCore/1-MapVote/7_GametypeRegistry.lua",
-
-	"4-ServerCore/2-ServerUtil/discord.lua",
-	"4-ServerCore/2-ServerUtil/VL_IntermissionLock-v1.lua",
-	"4-ServerCore/2-ServerUtil/admintools.lua",
-	"4-ServerCore/2-ServerUtil/SaveMap.lua",
-	"4-ServerCore/2-ServerUtil/LUA_MOTD",
 
     "1-GunMod/Rawset.lua",
 	"1-GunMod/Console.lua",
@@ -34,34 +60,14 @@ for _, filename in ipairs{
 	"1-GunMod/NewRingExplode.lua",
 	"1-GunMod/RocketForce.lua",
 	
+	"4-ServerCore/2-ServerUtil/discord.lua",
+	"4-ServerCore/2-ServerUtil/VL_IntermissionLock-v1.lua",
+	"4-ServerCore/2-ServerUtil/admintools.lua",
+	"4-ServerCore/2-ServerUtil/SaveMap.lua",
+	"4-ServerCore/2-ServerUtil/LUA_MOTD",
+	
 -- Zombie Escape Core Scripts 
 
-	"2-ZECore/1-Init/int_freeslot.lua",
-	"2-ZECore/1-Init/int_global.lua",
-	"2-ZECore/1-Init/int_ZE_RSNEO.lua", -- ZE custom weapons
-	
-	"2-ZECore/2-Console/con_timer.lua",
-	"2-ZECore/2-Console/con_rhealth.lua",
-	"2-ZECore/2-Console/con_cmds.lua", -- ze_spectate mainly here
-	
-	"2-ZECore/3-Library/lib_ZE_main.lua",
-	"2-ZECore/3-Library/lib_ZE_rhealth.lua",
-	"2-ZECore/3-Library/lib_ZE_rhealth_dmg.lua",
-	"2-ZECore/3-Library/lib_ZE_player_cfg.lua",
-	"2-ZECore/3-Library/lib_ZE_player_rhealth.lua",
-	"2-ZECore/3-Library/lib_ZE_player_misc.lua",
-	"2-ZECore/3-Library/lib_ZE_mapload.lua",
-	"2-ZECore/3-Library/lib_ZE_netvars.lua",
-	"2-ZECore/3-Library/lib_ZE_MobjCollide.lua",
-	"2-ZECore/3-Library/lib_ZE_propspawn.lua",
-	"2-ZECore/3-Library/lib_ZE_radar.lua",
-	"2-ZECore/3-Library/lib_ZE_ExtraChars.lua",
-	"2-ZECore/3-Library/lib_ZE_Ability_Milne.lua",
-	"2-ZECore/3-Library/lib_ZE_ExtraChars_exec.lua",
-	"2-ZECore/3-Library/lib_ZE_rhealth_Extra.lua",
-	"2-ZECore/3-Library/lib_ZE_Revenger.lua",
-	"2-ZECore/3-Library/lib_ZE_swarmhud.lua",
-	"2-ZECore/3-Library/lib_ZE_antibounce.lua",
 	
 	"3-LuaMap/LUA_SCPM",
 	"3-LuaMap/LUA_MP41",
@@ -70,7 +76,6 @@ for _, filename in ipairs{
 	"3-LuaMap/LUA_MP45",
 	"3-LuaMap/LUA_MP47",
 	"3-LuaMap/LUA_MP48",
-	
 } do
     dofile(filename)
 end
