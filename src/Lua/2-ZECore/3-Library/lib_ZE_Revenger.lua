4d8bac3eac514137650385c602c02a5ef02473a5
Blur <danielthesmartypants2017@gmail.com>
2023-03-22 01:14:12 -0500

    rvgrpass test

--- src/Lua/2-ZECore/3-Library/lib_ZE_Revenger.lua
+++ src/Lua/2-ZECore/3-Library/lib_ZE_Revenger.lua
@@ -50,8 +50,10 @@ ZE.Revenger = function(player)
 end
 
 ZE.UnlockRevenger = function(player)
-	chatprint(player.name + "\x8F\ Unlocked Revenger! " + "("+player.wins+" Wins met!"+")")
-	player.rvgrpass = 1
+	if player.rvgrpass ~= 1 then
+		chatprint(player.name + "\x8F\ Unlocked Revenger! " + "("+player.wins+" Wins met!"+")")
+		player.rvgrpass = 1
+	end
 end
 
 addHook("MapChange", do
