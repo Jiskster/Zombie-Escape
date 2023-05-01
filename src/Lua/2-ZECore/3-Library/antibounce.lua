761921c65abc49a94f12390fe5ac33eafe714b41
Blur <danielthesmartypants2017@gmail.com>
2023-05-01 01:35:27 -0500

    bounce before game start
    this fixes the no bounce bug.
    i feel like the game is getting more delayed

--- src/Lua/2-ZECore/3-Library/antibounce.lua
+++ src/Lua/2-ZECore/3-Library/antibounce.lua
@@ -1,9 +1,10 @@
 local ZE = RV_ZESCAPE
+local CV = RV_ZESCAPE.Console
 
 ZE.AntiBounce = function(player)
 	if not mapheaderinfo[gamemap].bounceoff then return end
 	
-	if player and player.mo and player.mo.valid then
+	if player and player.mo and player.mo.valid and CV.gamestarted then
 		if player.charability == CA_BOUNCE then
 			player.charability = CA_NONE
 		end
