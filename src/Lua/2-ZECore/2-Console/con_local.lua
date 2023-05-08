8016d7dc72c991dcf8e8705a94b3db7e19493031
Blur <danielthesmartypants2017@gmail.com>
2023-05-07 23:32:13 -0500

    hudtype and showendscore now saves locally

--- src/Lua/2-ZECore/2-Console/con_local.lua
+++ src/Lua/2-ZECore/2-Console/con_local.lua
@@ -1,13 +1,53 @@
 local CV = RV_ZESCAPE.Console
-
+local hudtypefilename = "client/ZombieEscape/hudtype.dat"
+local showendscorefilename = "client/ZombieEscape/showendscore.dat"
 CV.hudtype = CV_RegisterVar{
 	name = "hudtype",
 	defaultvalue = "2",
-	PossibleValue={MIN = 1, MAX = 2}
+	flags = CV_CALL|CV_NOINIT,
+	PossibleValue={MIN = 1, MAX = 2},
+	func = function(cv)
+		if io then
+			local file = io.openlocal(hudtypefilename, "w+")
+			file:write(cv.value)
+			file:close()
+		end
+	end
 }
 
 CV.showendscore = CV_RegisterVar{
 	name = "showendscore",
 	defaultvalue = "Off",
-	PossibleValue = CV_OnOff
+	flags = CV_CALL|CV_NOINIT,
+	PossibleValue = CV_OnOff,
+	func = function(cv)
+		if io then
+			local file = io.openlocal(showendscorefilename, "w+")
+			file:write(cv.value)
+			file:close()
+		end
+	end
 }
+local hudtypefile = io.openlocal(hudtypefilename)
+local hudtypevalue_fromfile
+local showendscorefile = io.openlocal(showendscorefilename)
+local showendscorevalue_fromfile
+if hudtypefile
+	local num = hudtypefile:read("*n")
+	hudtypevalue_fromfile = num
+	hudtypefile:close()
+end
+
+
+if showendscorefile
+	local num = showendscorefile:read("*n")
+	showendscorevalue_fromfile = num
+	showendscorefile:close()
+end
+if hudtypevalue_fromfile then
+	COM_BufInsertText(nil, "hudtype "..hudtypevalue_fromfile)
+end
+
+if showendscorevalue_fromfile then
+	COM_BufInsertText(nil, "showendscore "..showendscorevalue_fromfile)
+end
