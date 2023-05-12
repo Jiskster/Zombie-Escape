65bbce112ca363462542bac9d0c67cec12633cdc
Blur <danielthesmartypants2017@gmail.com>
2023-05-11 23:36:03 -0500

    more

--- src/init.lua
+++ src/init.lua
@@ -2,21 +2,21 @@ for _, filename in ipairs{
 -- Funny init
 
 	--first thing to load
-	"2-ZECore/1-Init/int_spawnobject.lua",
-	"2-ZECore/1-Init/int_showhitbox.lua",
-	"2-ZECore/1-Init/int_freeslot.lua",
-	"2-ZECore/1-Init/int_global.lua",
-	"2-ZECore/1-Init/int_ZE_RSNEO.lua",
+	"2-ZECore/init/int_spawnobject.lua",
+	"2-ZECore/init/int_showhitbox.lua",
+	"2-ZECore/init/int_freeslot.lua",
+	"2-ZECore/init/int_global.lua",
+	"2-ZECore/init/int_ZE_RSNEO.lua",
 
 
 	--cmds and etc
-	"2-ZECore/2-Console/con_timer.lua",
-	"2-ZECore/2-Console/con_cvars.lua",
-	"2-ZECore/2-Console/con_cmds.lua",
-	"2-ZECore/2-Console/con_local.lua",
+	"2-ZECore/console/con_timer.lua",
+	"2-ZECore/console/con_cvars.lua",
+	"2-ZECore/console/con_cmds.lua",
+	"2-ZECore/console/con_local.lua",
 	
 	--main ze code	
-	"2-ZECore/3-Library/main.lua",
+	"2-ZECore/maingame/main.lua",
 	"2-ZECore/3-Library/colors.lua",
 	"2-ZECore/3-Library/inputfeatures.lua",
 	"2-ZECore/3-Library/rhealth.lua",
@@ -24,9 +24,9 @@ for _, filename in ipairs{
 	"2-ZECore/3-Library/playerstats.lua",
 	"2-ZECore/3-Library/player.lua",
 	"2-ZECore/3-Library/name_tags.lua",
-	"2-ZECore/3-Library/mapload.lua",
-	"2-ZECore/3-Library/netvars.lua",
-	"2-ZECore/3-Library/mobjcollide.lua",
+	"2-ZECore/maingame/mapload.lua",
+	"2-ZECore/maingame/netvars.lua",
+	"2-ZECore/maingame/mobjcollide.lua",
 	"2-ZECore/3-Library/propspawn.lua",
 	"2-ZECore/3-Library/radar.lua",
 	"2-ZECore/3-Library/ability_milne.lua",
@@ -41,13 +41,13 @@ for _, filename in ipairs{
 	"2-ZECore/4-Execute/Exec_ZE_ExtraChars.lua",
 
 	--mapvote stuff
-	"3-ServerCore/1-MapVote/1_Rawset.lua",
-	"3-ServerCore/1-MapVote/2_Console.lua",
-	"3-ServerCore/1-MapVote/3_DebugFunctions.lua",
-	"3-ServerCore/1-MapVote/4_Functions.lua",
-	"3-ServerCore/1-MapVote/5_HUD.lua",
-	"3-ServerCore/1-MapVote/6_Main.lua",
-	"3-ServerCore/1-MapVote/7_GametypeRegistry.lua",
+	"4-CoreServer/1-MapVote/1_Rawset.lua",
+	"4-CoreServer/1-MapVote/2_Console.lua",
+	"4-CoreServer/1-MapVote/3_DebugFunctions.lua",
+	"4-CoreServer/1-MapVote/4_Functions.lua",
+	"4-CoreServer/1-MapVote/5_HUD.lua",
+	"4-CoreServer/1-MapVote/6_Main.lua",
+	"4-CoreServer/1-MapVote/7_GametypeRegistry.lua",
 
 	--Ringslinger neo base
     "1-GunMod/Rawset.lua",
@@ -71,21 +71,21 @@ for _, filename in ipairs{
 	"4-ZEMov/Mov_ZE_mobj.lua",
 	
 	--ze server base
-	"3-ServerCore/2-ServerUtil/discord.lua",
-	"3-ServerCore/2-ServerUtil/VL_IntermissionLock-v1.lua",
-	"3-ServerCore/2-ServerUtil/admintools.lua",
-	"3-ServerCore/2-ServerUtil/SaveMap.lua",
-	"3-ServerCore/2-ServerUtil/LUA_MOTD",
-	"3-ServerCore/2-ServerUtil/L_StuffAccounts_1.3f.lua",
+	"4-CoreServer/2-ServerUtil/discord.lua",
+	"4-CoreServer/2-ServerUtil/VL_IntermissionLock-v1.lua",
+	"4-CoreServer/2-ServerUtil/admintools.lua",
+	"4-CoreServer/2-ServerUtil/SaveMap.lua",
+	"4-CoreServer/2-ServerUtil/LUA_MOTD",
+	"4-CoreServer/2-ServerUtil/L_StuffAccounts_1.3f.lua",
 	
 	
-	"5-Maps/corruptedvoid.lua",
-	"5-Maps/minecraftv1.lua",
-	"5-Maps/scp.lua",
-	"5-Maps/secretlab2.lua",
-	"5-Maps/specialstage.lua",
-	"5-Maps/stonewood.lua",
-	"5-Maps/undertalept1.lua",
+	"3-Maps/corruptedvoid.lua",
+	"3-Maps/minecraftv1.lua",
+	"3-Maps/scp.lua",
+	"3-Maps/secretlab2.lua",
+	"3-Maps/specialstage.lua",
+	"3-Maps/stonewood.lua",
+	"3-Maps/undertalept1.lua",
 } do
     dofile(filename)
 end
