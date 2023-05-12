2ea9f3df80d2556d505ba3473a49e9ce88afc981
Blur <danielthesmartypants2017@gmail.com>
2023-05-12 07:59:34 -0500

    moar

--- src/Lua/2-Core/player/player.lua
+++ src/Lua/2-Core/player/player.lua
@@ -266,6 +266,16 @@ ZE.HealthLimit = function(player)
 	end
 end
 
+ZE.AntiBounce = function(player)
+	if not mapheaderinfo[gamemap].bounceoff then return end
+	
+	if player and player.mo and player.mo.valid and CV.gamestarted then
+		if player.charability == CA_BOUNCE then
+			player.charability = CA_NONE
+		end
+	end
+end
+
 ZE.AmyRegen = function(player)
 	local checkvars = player.mo.maxHealth and player.mo.health
     if not (gametype == GT_ZESCAPE) return end
